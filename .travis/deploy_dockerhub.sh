17aba3ac2575716fc883c35d54610de4c30e01a5
valerian <valeriansokolovskiy@gmail.com>
2019-12-24 16:29:58 +0200

    fix security

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS docker.io
-#docker login -u "valerians777" -p "Valerian301331" docker.io
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
@@ -9,6 +8,4 @@ else
 fi
 
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-#docker build -f Dockerfile -t valerians777/cicd-buzz:$TAG .
-#docker push valerians777/cicd-buzz
 docker push $TRAVIS_REPO_SLUG
