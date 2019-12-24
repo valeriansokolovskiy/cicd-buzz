1ab580760d88112f52401bc2bdca318a670dcd4a
valerian <valeriansokolovskiy@gmail.com>
2019-12-24 17:33:37 +0200

    fix heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -5,5 +5,6 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 docker login  -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web3 --app $HEROKU_APP_NAME
 heroku container:release web3 --app $HEROKU_APP_NAME
+heroku ps:scale web3=1
 #heroku ps:scale web3=1
 
